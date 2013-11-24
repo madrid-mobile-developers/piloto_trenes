080bc1e61b73e9e0a76274c45b43c5378dae05ea
Javier Arias <javierarias@gmx.es>
2013-11-24 20:42:45 +0100

    Deleting extra files from seed

--- webapp/routes/routes.js
+++
@@ -1,9 +0,0 @@
-var passport = require('passport');
-
-module.exports = function (app) {
-    
-    app.get('/', function (req, res) {
-        res.render('login', { user : req.user });
-    });
-    
-};
