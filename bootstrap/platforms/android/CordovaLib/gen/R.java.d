dc6a7ff4fc11d0060d5e06f14c15de342e39c64c
Javier Arias <javierarias@gmx.es>
2014-01-09 11:47:53 +0100

    Bootstrap projects

---
+++ bootstrap/platforms/android/CordovaLib/gen/R.java.d
@@ -0,0 +1,1 @@
+ : /Users/jarias/Desarrollos/Workspaces/WS_Sanitas/piloto_trenes/bootstrap/platforms/android/CordovaLib/bin/AndroidManifest.xml \
---
+++ bootstrap/platforms/android/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/jarias/Desarrollos/Workspaces/WS_Sanitas/piloto_trenes/bootstrap/platforms/android/bin/AndroidManifest.xml : \
+/Users/jarias/Desarrollos/Workspaces/WS_Sanitas/piloto_trenes/bootstrap/platforms/android/AndroidManifest.xml \
