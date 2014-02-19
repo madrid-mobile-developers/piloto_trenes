da5d0c7bb8359e0c24839ca668eeef77bd568c12
Javier Arias <javierarias@gmx.es>
2014-02-19 17:14:34 +0100

    Including cordova geolocation plugins

--- webapp/src/js/views/StationsList.js
+++ webapp/src/js/views/StationsList.js
@@ -9,13 +9,38 @@ define('views/StationsList',
         'models/StationsModel',
         'text!../../partials/stationsList.html'
     ],
-    function(_, Backbone, StationsModel, tpl){
+    function (_, Backbone, StationsModel, tpl) {
         var View = Backbone.View.extend({
             el: '#viewContainer',
-            initialize: function(){
+            initialize: function () {
                 this.model = new StationsModel();
             },
-            render: function(){
+            render: function () {
+                var long = '',
+                    lat = '';
+                var showGeolocation = function(){
+                    navigator.geolocation.getCurrentPosition(
+                        function (position) {
+                            lat = position.coords.latitude;
+                            long = position.coords.longitude;
+                            //In case the fetch has been finished
+                            $('#lat').html(position.coords.latitude);
+                            $('#long').html(position.coords.longitude);
+                            console.log("Lat: " + position.coords.latitude + " Long: " + position.coords.longitude);
+                        },
+                        function (error) {
+                            $('#lat').parent.hide();
+                            console.log('code: ' + error.code + '\n' +
+                                'message: ' + error.message + '\n');
+                        });
+                };
+                if (navigator && navigator.geolocation && navigator.geolocation.getCurrentPosition) {
+                    showGeolocation();
+                }else{
+                    window.addEventListener('deviceready', showGeolocation);
+                }
+
+
                 //The deferred object
                 var deferred = new $.Deferred();
                 //div where the view will be rendered
@@ -24,15 +49,16 @@ define('views/StationsList',
                 var spliceFunction = this.spliceStations;
                 //This will be a fetch but in the seed we'll fill it with static data
                 this.model.fetch({
-                    success : function(model){
+                    success: function (model) {
                         //Include in DOM the template
                         //In order to improve the POC we are slicing the results array
-                        container.html(_.template(tpl, {data: spliceFunction(model)['Stations']}));
+                        container.html(_.template(tpl, {data: spliceFunction(model)['Stations'], long:long, lat:lat}));
                         //Hide loading
                         $.mobile.hidePageLoadingMsg();
+
                         deferred.resolve();
                     },
-                    error : function(collection, response){
+                    error: function (collection, response) {
                         //Hide loading
                         $.mobile.hidePageLoadingMsg();
                         //Include in DOM the template
@@ -41,16 +67,15 @@ define('views/StationsList',
                     }
                 });
                 return deferred.promise();
-            }
-            ,
+            },
             /**
              * It return only 15 stations
              * @param model
              */
-            spliceStations: function(model){
+            spliceStations: function (model) {
                 var stationsList = model.get('Stations');
-                if(stationsList.length >15){
-                    stationsList = stationsList.splice(0,15);
+                if (stationsList.length > 15) {
+                    stationsList = stationsList.splice(0, 15);
                     model.set('Stations', stationsList);
                 }
                 return model.toJSON();
